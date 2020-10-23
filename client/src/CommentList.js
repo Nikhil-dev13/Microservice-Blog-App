b408a8296b6bafe281a1e163357422dd95c726d0
Nikhil-dev13 <nikhil13012000@gmail.com>
2020-10-24 00:04:39 +0530

    Complete react components

---
+++ client/src/CommentList.js
@@ -0,0 +1,26 @@
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchData = async () => {
+    const res = await Axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;

40b114021a52a1bdc0ae4d52aa6fae50c773d905
Nikhil-dev13 <nikhil13012000@gmail.com>
2020-10-24 22:44:58 +0530

    add query in react

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,21 +1,7 @@
-import Axios from "axios";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await Axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+const CommentList = ({ comments }) => {
+  //render function
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
